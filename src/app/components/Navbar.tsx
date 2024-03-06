1a6d0c52930cc27ae782f7d27c2e0521222dfe95
Fernando Ferreira <feernnando.f.filho@gmail.com>
2024-03-06 12:30:06 -0300

    Adicionando autenticação com o Clerk

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,16 +1,30 @@
 import Link from "next/link";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 
 function Navbar() {
-    return (
-        <nav className="fixed top-0 w-full flex items-center py-2 px-8 justify-between z-50 bg-slate-800 text-gray-200">
-            <Link href="/"
-
-                className='uppercase font-bold text-md h-12 flex items-center'
-            >
-                Next Store
-            </Link>
-        </nav>
-    )
+  return (
+    <nav className="fixed top-0 w-full flex items-center py-2 px-8 justify-between z-50 bg-slate-800 text-gray-200">
+      <Link
+        href="/"
+        className="uppercase font-bold text-md h-12 flex items-center"
+      >
+        Next Store
+      </Link>
+      <div className="flex items-center gap-8">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <button className="border rounded-md border-gray-400 px-3 py-2">
+              Realizar Login
+            </button>
+          </SignInButton>
+        </SignedOut>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
